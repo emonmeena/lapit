fe24f64a4187304141ef70902d165783115d9329
mayank-genesis <mayank16meena@gmail.com>
2020-06-14 23:08:55 +0530

    Add RealTimeDatabase

--- app/src/main/java/com/example/android/lapitchat/RegisterActivity.java
+++ app/src/main/java/com/example/android/lapitchat/RegisterActivity.java
@@ -17,6 +17,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -26,6 +31,8 @@ public class RegisterActivity extends AppCompatActivity {
     private Button mCreateBtn;
 
     private Toolbar mToolbar;
+
+    private DatabaseReference mDatabase;
 
     //ProgressDialog
     private ProgressDialog mRegProgress;
@@ -73,17 +80,38 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    private void register_user(String user_name, String email, String password) {
+    private void register_user(final String user_name, String email, String password) {
        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
            @Override
            public void onComplete(@NonNull Task<AuthResult> task) {
             if (task.isSuccessful()){
-                mRegProgress.dismiss();
 
-                Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
-                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(mainIntent);
-                finish();
+                FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+                String uid = current_user.getUid();
+
+                mDatabase = FirebaseDatabase.getInstance().getReference().child("user").child(uid);
+
+                HashMap<String, String> userMap = new HashMap<>();
+                userMap.put("name", user_name);
+                userMap.put("status", "Hi there I am using LapitChat App");
+                userMap.put("image", "default");
+                userMap.put("thumb_image", "default");
+
+                mDatabase.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            mRegProgress.dismiss();
+
+                            Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(mainIntent);
+                            finish();
+                        }
+                    }
+                });
+
+
             }
             else{
                 mRegProgress.hide();
---
+++ app/src/main/java/com/example/android/lapitchat/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.lapitchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
